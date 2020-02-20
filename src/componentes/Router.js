678422942ca17dc9c91a0e874eb48ce29c069f9b
= <=>
2020-02-20 12:05:57 +0000

    router añadido

---
+++ src/componentes/Router.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import Login from './Login';
+
+class Router extends Component{
+    render(){
+        return(
+            <BrowserRouter>
+                <Switch>                    
+                    <Route exact path="/" component={Login} />
+                    <Route exact path="/login" component={Login} />   
+                    <Route exact path="/home" component={Login} />          
+                </Switch>
+            </BrowserRouter>
+        );
+    }
+}
+
+export default Router;
