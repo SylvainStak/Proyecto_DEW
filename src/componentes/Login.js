7f2ac2d88519d6cd7460ea3da20f68ef90b47909
SylvainStak <stackersap@gmail.com>
2020-02-28 16:36:24 +0000

    logins añadidos menos telefono

--- src/componentes/Login.js
+++ src/componentes/Login.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
+import withFirebaseAuth from 'react-with-firebase-auth';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseCfg from '../firebase';
 import '../css/login.css';
 
-class Login extends Component{
+const firebaseApp = firebase.initializeApp(firebaseCfg);
+
+class Login extends Component{ 
+     
+
     showPhoneModal(){        
         console.log('hola');
     }
 
     render(){
+        const {
+            user,
+            signOut,
+            signInWithGoogle,
+            signInWithTwitter,
+            signInWithGithub,
+        } = this.props;  
+
+        
+
         return(
             <div id="login_card">
                 <h3 className="text-center">Inicia Sesion</h3>
@@ -21,15 +39,33 @@ class Login extends Component{
 
                 <label>También puede iniciar sesión con:</label><br/>
                 <div className="d-flex justify-content-around w-25 m-auto">
-                <span id="googleLogin">
-                    <i className="fab fa-google fa-3x text-danger"></i>
-                </span>
-                <span id="twitterLogin">
-                    <i className="fab fa-twitter fa-3x text-primary"></i>
-                </span>
-                <span id="githubLogin">
-                    <i className="fab fa-github fa-3x"></i>
-                </span>
+                {
+                !user
+                    ? <span id="googleLogin" onClick={signInWithGithub}>
+                        <i className="fab fa-google fa-3x text-danger"></i>
+                    </span>
+                    : ''
+                }
+                {
+                !user
+                    ? <span id="twitterLogin" onClick={signInWithTwitter}>
+                        <i className="fab fa-twitter fa-3x text-primary"></i>
+                    </span>
+                    : ''
+                }
+                {
+                !user
+                    ? <span id="githubLogin" onClick={signInWithGithub}>
+                        <i className="fab fa-github fa-3x"></i>
+                    </span>
+                    : ''
+                }
+                {
+                    user ?
+                    <button onClick={signOut}>Sign Out</button>:
+                    ''
+                }
+                
                 <span id="phoneLogin" onClick={this.showPhoneModal}>
                     <i className="fas fa-phone fa-3x text-success"></i>
                 </span>
@@ -43,4 +79,17 @@ class Login extends Component{
     }
 }
 
-export default Login;
+const firebaseAppAuth = firebaseApp.auth();
+
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
+  twitterProvider: new firebase.auth.TwitterAuthProvider(),
+  githubProvider: new firebase.auth.GithubAuthProvider()
+};
+
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+})(Login);
+
+//export default Login;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+export default{
+    apiKey: "AIzaSyDXH9SffIOVmOUB86JXe9219Rp4bzCNu_I",
+    authDomain: "filtros-y-mapas.firebaseapp.com",
+    databaseURL: "https://filtros-y-mapas.firebaseio.com",
+    projectId: "filtros-y-mapas",
+    storageBucket: "filtros-y-mapas.appspot.com",
+    messagingSenderId: "510530021035",
+    appId: "1:510530021035:web:09a3da2c54587f340039ee"
+}
